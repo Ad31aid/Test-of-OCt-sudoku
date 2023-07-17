a43ab3cdae54d4288587ce73ff221524c68d34ac
Ad31ide <seanchendu@gmail.com>
2023-07-17 03:27:42 -0500

    somenewstuff

--- gp1back/script.js
+++ gp1back/script.js
@@ -3,13 +3,52 @@
 
 //define raw API data for storage
 var raw;
+//this raw doesn't do anything, i don't know why
 
   async function gainsheet() {
     const response = await fetch("https://sudoku-api.vercel.app/api/dosuku");
     const rawAPI = await response.json();
-    console.log(rawAPI.newboard.grids[0].solution); raw = rawAPI;
+    console.log(rawAPI)
+      let boardSolution = rawAPI.newboard.grids[0].solution;
+      let boardValue = rawAPI.newboard.grids[0].value;
+      console.log(boardValue)
+      //nest function/or call back?
+      bb => {for (let i = 1; i <= 9; i++){
+        //<div id=i class number> i <div>
+        let numberBox = document.createElement('div')
+        numberBox.id = i
+        numberBox.innerText = i
+        numberBox.addEventListener('click', ()=>console.log('jeb'))
+        numberBox.classList.add('number')
+        document.getElementById('digits').appendChild(numberBox)
+      }
+    
+      //board 9x9, easy!
+      for(let r = 1; r <= 9; r++){
+        //each row and each col
+        for(let c = 1; c <= 9; c++){
+          let box = document.createElement('div');
+          box.id = r.toString() + '-' + c.toString();
+          if(boardValue[r][c] != '0'){
+            box.innerText = boardvalue[r][c]
+            box.classList.add('box-start')
+            //skipping 0 in the begining value
+          }
+          if (r == 2 || r==5){
+            box.classList.add('horizontal-line')}
+          if (c == 2 || c == 5) {
+            box.classList.add("vertical-line");
+            //add line
+          }
+          box.addEventListener("click", ()=>console.log('jebjeb'));
+          box.classList.add("box");
+          document.getElementById("board").append(box);
+        }
+      }}
   }
 
+
+ 
 
 
 // async function gainRaw(){
@@ -22,81 +61,70 @@ var raw;
 
 // have no fucking idea why doesn't the code above work
 
+//third Try!
+// fetch('https://sudoku-api.vercel.app/api/dosuku')
+//   .then(res => {
+//     return res.json();
+//   })
+//   .then(raw => {
+//     console.log(raw);
+//     newBoard => {
+//       raw.newboard.grid[0];
+//       console.log(newBoard)
+//     }
+//     soluion => {
+//     }
+//   })
+//   .catch( error => console.log(error))
+
 //asign cordnite for number selected also tile that is selected
     //error feature for scoring
     var numberSelected = null
     var tileSelected = null;
     var error = 0;
-
-    gainsheet()
-
-console.log(raw)
-
-
 //trying to fucking get the board, it doesn't let me extract the property of raw what the heck
 
 
-//   var board = [
-//       "--74916-5",
-//       "2---6-3-9",
-//       "-----7-1-",
-//       "-586----4",
-//       "--3----9-",
-//       "--62--187",
-//       "9-4-7---2",
-//       "67-83----",
-//       "81--45---"
-//   ]
+ //start game once window load 
+window.onload = gainsheet()
+
   
-//   var solution = [
-//       "387491625",
-//       "241568379",
-//       "569327418",
-//       "758619234",
-//       "123784596",
-//       "496253187",
-//       "934176852",
-//       "675832941",
-//       "812945763"
-//   ]
-  
-//   window.onload = function() {
-//       setGame();
-//   }
-  
-//   function setGame() {
-//       // Digits 1-9
-//       for (let i = 1; i <= 9; i++) {
-//           //<div id="1" class="number">1</div>
-//           let number = document.createElement("div");
-//           number.id = i
-//           number.innerText = i;
-//           number.addEventListener("click", selectNumber);
-//           number.classList.add("number");
-//           document.getElementById("digits").appendChild(number);
-//       }
-  
-//       // Board 9x9
-//       for (let r = 0; r < 9; r++) {
-//           for (let c = 0; c < 9; c++) {
-//               let tile = document.createElement("div");
-//               tile.id = r.toString() + "-" + c.toString();
-//               if (board[r][c] != "-") {
-//                   tile.innerText = board[r][c];
-//                   tile.classList.add("tile-start");
-//               }
-//               if (r == 2 || r == 5) {
-//                   tile.classList.add("horizontal-line");
-//               }
-//               if (c == 2 || c == 5) {
-//                   tile.classList.add("vertical-line");
-//               }
-//               tile.addEventListener("click", selectTile);
-//               tile.classList.add("tile");
-//               document.getElementById("board").append(tile);
-//           }
-//       }
-//   }
+function setGame(){
+  //digit 1-9 input bar
+  for (let i = 1; i <= 9; i++){
+    //<div id=i class number> i <div>
+    let numberBox = document.createElement('div')
+    numberBox.id = i
+    numberBox.innerText = i
+    numberBox.addEventListener('click', ()=>console.log('jeb'))
+    numberBox.classList.add('number')
+    document.getElementById('digits').appendChild(numberBox)
+  }
+
+  //board 9x9, easy!
+  for(let r = 1; r <= 9; r++){
+    //each row and each col
+    for(let c = 1; c <= 9; c++){
+      let box = document.createElement('div');
+      box.id = r.toString() + '-' + c.toString();
+      if(boardValue[r][c] != '0'){
+        box.innerText = boardvalue[r][c]
+        box.classList.add('box-start')
+        //skipping 0 in the begining value
+      }
+      if (r == 2 || r==5){
+        box.classList.add('horizontal-line')}
+      if (c == 2 || c == 5) {
+        box.classList.add("vertical-line");
+        //add line
+      }
+      box.addEventListener("click", ()=>console.log('jebjeb'));
+      box.classList.add("box");
+      document.getElementById("board").append(box);
+    }
+  }
+}
+
   
 //   function selectNumber(){
 //       if (numSelected != null) {
